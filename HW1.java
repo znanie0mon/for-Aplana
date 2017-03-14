853d0e6ea2eeaaedaa6c0d83da5844ff0c93ddab
znanie0mon <znanie2117@gmail.com>
2017-03-14 18:39:42 +0300

    Create HW1.java
    1 задание

---
+++ HW1.java
@@ -0,0 +1,31 @@
+ import java.util.Scanner;
+ import java.util.*
+ 
+ public class Program 1 {
+    
+    public static void Main(String[] args) {
+ 
+ /*1 задание - В переменных q и w хранятся два натуральных числа. Создайте программу, выводящую на экран результат деления q на w с остатком.
+        Пример вывода программы (для случая, когда в q хранится 21, а в w хранится 8): 21 / 8 = 2 и 5 в остатке.
+         *
+
+        int rez;
+        int q;
+        int w;
+        int ostatok;
+
+        System.out.println(" Введите первое число ");
+        q = inputInteger();
+
+        System.out.println(" Введите второе число ");
+        w = inputInteger();
+
+        rez = q/w;
+        ostatok = q%w;
+
+        System.out.println("для случая, когда в q хранится " + q +
+                " а в w хранится " + w + "):" + q + "/" + w + "=" +  rez + " и " + ostatok +  " в остатке.");
+        }
+                
+                
+       }
